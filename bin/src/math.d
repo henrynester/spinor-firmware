3e12a744390491ae389761a6e22622897b8fe013
Henry Nester <henrynester@gmail.com>
2025-06-01 20:16:16 -0400

    yes

--- bin/src/math.d
+++
@@ -1,3 +0,0 @@
-bin/src/math.o: src/math.c src/math.h \
- /usr/lib/gcc/arm-none-eabi/12.2.1/include/stdint.h \
- /usr/lib/gcc/arm-none-eabi/12.2.1/include/arm_fp16.h
--- bin/src/thermistor.d
+++
@@ -1,2 +0,0 @@
-bin/src/thermistor.o: src/thermistor.c src/thermistor.h \
- /usr/lib/gcc/arm-none-eabi/12.2.1/include/stdint.h
